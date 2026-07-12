f8e958cf8c22500ff588fe216e7248f404febde6
Elis Rodrigues <elisrb2004@gmail.com>
2026-07-12 18:03:39 -0300

    atualiza sprites

---
+++ sprites/fundos.h
@@ -0,0 +1,10 @@
+#ifndef FUNDOS_H
+#define FUNDOS_H
+
+const unsigned short rota1[576][320];
+const unsigned short casa1[128][128];
+const unsigned short casa2[128][128];
+const unsigned short casa_rival[128][128];
+const unsigned short lab[192][160];
+
+#endif // FUNDOS_H
